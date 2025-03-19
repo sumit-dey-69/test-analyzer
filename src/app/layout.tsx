b2bf78d2c1258bb5e0a505b755099d4f9b4a18d8
sumit-dey-69 <sumitdey.dev@gmail.com>
2025-03-19 14:06:04 +0530

    chore:  refactor layout and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto_mono = Roboto_Mono({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  display: "swap",
+  style: "normal",
+  variable: "--font-roboto-mono",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${roboto_mono.variable} font-roboto-mono antialiased`}>{children}</body>
     </html>
   );
 }
