87c22e4bd6a88c7f8ac41984d15d40526a009024
sumit-dey-69 <sumitdey.dev@gmail.com>
2025-03-20 10:40:27 +0530

    feat: implement Zustand store for managing test state and answers

---
+++ src/app/zustand/store.ts
@@ -0,0 +1,71 @@
+import { create } from "zustand";
+
+export type AnswerType = "Correct" | "Incorrect" | "Unattempt" | null;
+export type SubjectType = "Maths" | "Computer" | "Reasoning" | null;
+
+type Store = {
+  testCode: string;
+  setTestCode: (code: string) => void;
+  answers: Record<number, AnswerType>;
+  setAnswers: (index: number, answer: AnswerType) => void;
+  subjects: Record<number, SubjectType>;
+  setSubjects: (index: number, subject: SubjectType) => void;
+  reset: () => void;
+  correctCount: () => number;
+  incorrectCount: () => number;
+  unattemptCount: () => number;
+};
+
+const useStore = create<Store>((set, get) => ({
+  testCode: "",
+  answers: {},
+  subjects: {},
+
+  setTestCode: (code) => set({ testCode: code }),
+
+  setAnswers: (index, answer) => {
+    set((state) => ({
+      answers: {
+        ...state.answers,
+        [index]: answer,
+      },
+    }));
+  },
+  setSubjects: (index, subject) => {
+    set((state) => ({
+      subjects: {
+        ...state.subjects,
+        [index]: subject,
+      },
+    }));
+  },
+  reset: () => {
+    set({
+      answers: {},
+      subjects: {},
+    });
+  },
+
+  correctCount: () =>
+    Object.values(get().answers).filter((answer) => answer === "Correct")
+      .length,
+  incorrectCount: () =>
+    Object.values(get().answers).filter((answer) => answer === "Incorrect")
+      .length,
+  unattemptCount: () =>
+    Object.values(get().answers).filter((answer) => answer === "Unattempt")
+      .length,
+}));
+
+export const useTestCode = () => useStore((state) => state.testCode);
+export const useSetTestCode = () => useStore((state) => state.setTestCode);
+export const useAnswers = () => useStore((state) => state.answers);
+export const useSetAnswers = () => useStore((state) => state.setAnswers);
+export const useSubjects = () => useStore((state) => state.subjects);
+export const useSetSubjects = () => useStore((state) => state.setSubjects);
+export const useReset = () => useStore((state) => state.reset);
+export const useCorrectCount = () => useStore((state) => state.correctCount);
+export const useIncorrectCount = () =>
+  useStore((state) => state.incorrectCount);
+export const useUnattemptCount = () =>
+  useStore((state) => state.unattemptCount);
